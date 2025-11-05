e5cff0f9b17a80859299eac39a45ebb944c61055
Evan <88143635+evan052006@users.noreply.github.com>
2025-11-05 13:41:49 +0700

    Finished Tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:football_news/widgets/left_drawer.dart';
+import 'package:football_news/widgets/item_card.dart';
+import 'package:football_news/widgets/info_card.dart';
 
 class MyHomePage extends StatelessWidget {
   final String nama = "Christopher Evan Tanuwidjaja";
   final String npm = "2406358056";
   final String kelas = "A";
-  static final List<ItemHomepage> items = [
+  final List<ItemHomepage> items = [
     ItemHomepage("See Football News", Icons.newspaper),
     ItemHomepage("Add News", Icons.add),
     ItemHomepage("Logout", Icons.logout),
   ];
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Football News',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      drawer: LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -48,7 +49,7 @@ class MyHomePage extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
                       ),
-                    )
+                    ),
                   ),
                   GridView.count(
                     primary: true,
@@ -60,7 +61,7 @@ class MyHomePage extends StatelessWidget {
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
-                  )
+                  ),
                 ],
               ),
             ),
@@ -71,32 +72,6 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class InfoCard extends StatelessWidget {
-  final String title;
-  final String content;
-  const InfoCard({super.key, required this.title, required this.content});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 2.0,
-      child: Container(
-        width: MediaQuery.of(context).size.width / 3.5,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8.0),
-            Text(content),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class ItemHomepage {
   final String name;
@@ -104,36 +79,3 @@ class ItemHomepage {
   ItemHomepage(this.name, this.icon);
 }
 
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-  const ItemCard(this.item, {super.key});
-
-  @override
-    Widget build(BuildContext context) {
-      return Material(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(12),
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        )
-      );
-    }
-}
